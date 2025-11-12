b52c8199241c4fc49f7a6a432a9a862c016d674c
David <david_ofernandez@alumnos-santagema.es>
2025-11-12 15:38:14 +0100

    Cuenta bar finalizada

--- Gestioncuentavar/app/src/main/java/com/example/gestioncuentavar/MainActivity.java
+++ Gestioncuentavar/app/src/main/java/com/example/gestioncuentavar/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.gestioncuentavar;
 
+import android.graphics.Color;
+import android.media.Rating;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -22,6 +29,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        String[] opciones = {"Enrique","Adriana","Pablo","Eustaquio"};
+        AutoCompleteTextView autoCompleteNombreCamarero = findViewById(R.id.autoCompleteNombreCamarero);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, opciones);
+        autoCompleteNombreCamarero.setAdapter(adapter);
 
         TextView textViewPropina = findViewById(R.id.textViewPropina);
         SeekBar seekbarPropina = findViewById(R.id.seekbarPropina);
@@ -49,6 +62,8 @@ public class MainActivity extends AppCompatActivity {
         TextView textViewCalculo = findViewById(R.id.textViewCalculo);
         EditText textViewCuenta = findViewById(R.id.editTextCuenta);
         CheckBox checkBoxPropina = findViewById(R.id.checkBoxPropina);
+        RadioGroup radioGroup = findViewById(R.id.radioGroupMetodoPago);
+        RatingBar ratingBar = findViewById(R.id.ratingBar);
 
         button.setOnClickListener(new View.OnClickListener()
         {
@@ -61,23 +76,47 @@ public class MainActivity extends AppCompatActivity {
                 String textoCuenta = textViewCuenta.getText().toString();
                 if (textoCuenta.isEmpty())
                 {
-                    textViewCalculo.append("No se ha introducido una cuenta");
+                    textViewCalculo.setTextColor(Color.RED);
+                    textViewCalculo.append("No se ha introducido la cuenta");
                     return;
                 }
                 float num = Float.parseFloat(textoCuenta);
 
                 if (num <= 0)
                 {
+                    textViewCalculo.setTextColor(Color.RED);
                     textViewCalculo.append("Cuenta inválida");
                     return;
                 }
+
+                int id = radioGroup.getCheckedRadioButtonId();
+
+                if (id == -1)
+                {
+                    textViewCalculo.setTextColor(Color.RED);
+                    textViewCalculo.append("No se ha introducido un método de pago");
+                    return;
+                }
+
+                textViewCalculo.setTextColor(Color.BLACK);
                 textViewCalculo.append("Cuenta: " + textViewCuenta.getText() + "€\n");
 
                 if (checkBoxPropina.isChecked())
                 {
                     textViewCalculo.append("Propina: " + seekbarPropina.getProgress() + "%\n");
                     float res = num + (num * seekbarPropina.getProgress() / 100);
-                    textViewCalculo.append("Total: " + res + '\n');
+                    textViewCalculo.append("Total: " + res + "€\n");
+                }
+
+                RadioButton radioButton = findViewById(id);
+                textViewCalculo.append("Método de pago elegido: " + radioButton.getText() +'\n');
+
+                textViewCalculo.append("Calificación del servicio: " + ((int) ratingBar.getRating()) + " estrellas.");
+
+                String nombreCamarero = autoCompleteNombreCamarero.getText().toString();
+                if (!nombreCamarero.isEmpty())
+                {
+                    textViewCalculo.append("\nCamarero que ha atendido: "+nombreCamarero);
                 }
             }
         });
