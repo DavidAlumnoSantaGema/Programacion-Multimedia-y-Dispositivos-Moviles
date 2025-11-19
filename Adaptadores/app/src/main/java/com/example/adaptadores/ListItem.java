5d93c7fd2f4fbe04f6665b7580c0c9bee8629e6a
David <david_ofernandez@alumnos-santagema.es>
2025-11-19 20:48:45 +0100

    A

---
+++ Adaptadores/app/src/main/java/com/example/adaptadores/ListItem.java
@@ -0,0 +1,28 @@
+package com.example.adaptadores;
+
+public class ListItem
+{
+    private int imageResId;
+    private String title;
+    private String content;
+
+    public ListItem(int imageResId, String title, String content)
+    {
+        this.imageResId = imageResId;
+        this.title = title;
+        this.content = content;
+    }
+
+
+    public int getImageResId() {
+        return imageResId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ Adaptadores/app/src/main/java/com/example/adaptadores/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.adaptadores;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+{
+    private ListView listView;
+    private CustomAdapter adapter;
+    private List<ListItem> items;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+
+        items = new ArrayList<>();
+        items.add(new ListItem(R.drawable.ic_launcher_foreground, "Titulo 1", "Contenido del elemento 1"));
+        items.add(new ListItem(R.drawable.ic_launcher_foreground, "Titulo 2", "Contenido del elemento 2"));
+        items.add(new ListItem(R.drawable.ic_launcher_foreground, "Titulo 3", "Contenido del elemento 3"));
+        items.add(new ListItem(R.drawable.ic_launcher_foreground, "Titulo 4", "Contenido del elemento 4"));
+
+        adapter = new CustomAdapter(this, items);
+        listView.setAdapter(adapter);
+    }
+}
